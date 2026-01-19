976dba99b72e1131c79f74dca30c9c5d8d05c6ef
Jahid Islam Rafsan <rafsanx@CWs-Mac-mini.local>
2026-01-19 16:00:16 +0600

    done login sign up page

---
+++ js/login.js
@@ -0,0 +1,11 @@
+const eyeButton = document.querySelector("#eyeButton");
+const passwordInput = document.querySelector("#pass");
+
+eyeButton.addEventListener("click", () => {
+  const isPassword = passwordInput.type === "password";
+
+  passwordInput.type = isPassword ? "text" : "password";
+
+  eyeButton.classList.toggle("fa-eye", !isPassword);
+  eyeButton.classList.toggle("fa-eye-slash", isPassword);
+});
